019bf968edda9c3e52f5733f9993e1288ecce8ca
AristidesAI <aristideslintzeris@icloud.com>
2026-02-16 22:13:34 +1100

    Initial Commit

---
+++ VisionEmoji/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionEmoji
+//
+//  Created by aristides lintzeris on 16/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionEmoji/VisionEmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionEmojiApp.swift
+//  VisionEmoji
+//
+//  Created by aristides lintzeris on 16/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct VisionEmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
